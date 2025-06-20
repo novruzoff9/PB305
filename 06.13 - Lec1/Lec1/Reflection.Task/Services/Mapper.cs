090e33cfba8415a31010279ddc8fedca6156a61b
User <yagmurnov2005@gmail.com>
2025-06-20 12:37:00 +0400

    Changed Folder Structure

---
+++ 06.13 - Lec1/Lec1/Reflection.Task/Services/Mapper.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Reflection.Task.Services;
+
+public static class Mapper
+{
+    //                              User,      UserDto
+    public static TDestination Map<TSource, TDestination>(TSource source)
+        where TDestination : new()
+        where TSource : new()
+    {
+        TDestination destination = new();
+        PropertyInfo[] sourceProps = typeof(TSource).GetProperties();
+        PropertyInfo[] destinationProps = typeof(TDestination).GetProperties();
+
+        foreach (var sourceProp in sourceProps)
+        {
+            var destProp = destinationProps
+                .FirstOrDefault(x => x.Name == sourceProp.Name && x.PropertyType == sourceProp.PropertyType);
+            if(destProp != null && destProp.CanWrite)
+            {
+                object value = sourceProp.GetValue(source);
+                destProp.SetValue(destination, value);
+            }
+        }
+        return destination;
+    }
+}
